12e484092c39ef3c7122e4e36969cb6e3157646c
chenhaizhu <394279480@qq.com>
2024-05-13 22:02:30 +0800

    事件的学习

---
+++ react_study/04_Learn_Log/07_使用组件完成练习/Components/Logs/Logs.js
@@ -0,0 +1,16 @@
+import LogItem from "./LogItem/LogItem";
+import "./Logs.css";
+
+// 日志容器
+const Logs = () => {
+  return (
+    <div className="logs">
+      <LogItem />
+      <LogItem />
+      <LogItem />
+      <LogItem />
+    </div>
+  );
+};
+
+export default Logs;
