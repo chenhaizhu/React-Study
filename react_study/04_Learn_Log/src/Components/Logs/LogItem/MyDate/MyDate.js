75a686ed6b481d182238cbb524b424de37b082bd
chenhaizhu <394279480@qq.com>
2024-05-14 22:23:01 +0800

    联系项目的日期设置

--- react_study/04_Learn_Log/src/Components/Logs/LogItem/MyDate/MyDate.js
+++ react_study/04_Learn_Log/src/Components/Logs/LogItem/MyDate/MyDate.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./MyDate.css";
 
-const MyDate = () => {
+const MyDate = (props) => {
+  // 获取月份
+  const month = props.date.toLocaleString("zh-CN", { month: "long" });
+  // 获取日期
+  const date = props.date.getDate();
+
   return (
     <div>
       <div className="date">
-        <div className="month">四月</div>
-        <div className="day">19</div>
+        <div className="month">{month}</div>
+        <div className="day">{date}</div>
       </div>
     </div>
   );
--- react_study/04_Learn_Log/src/Components/Logs/Logs.js
+++ react_study/04_Learn_Log/src/Components/Logs/Logs.js
@@ -7,8 +7,12 @@ const Logs = () => {
     <div className="logs">
       {/* 在父组件中可以直接在子组件中设置属性 */}
       {/* <LogItem test="123" hello="abc" fn={() => {}} /> */}
-      <LogItem date={new Date()} desc={"学习JavaScript"} time={"50"} />
-      <LogItem date={new Date()} desc={"学习React"} time={"30"} />
+      <LogItem
+        date={new Date(2024, 5, 13)}
+        desc={"学习JavaScript"}
+        time={"50"}
+      />
+      <LogItem date={new Date(2024, 7, 5)} desc={"学习React"} time={"30"} />
     </div>
   );
 };
