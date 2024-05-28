564454923996d47976e12f8642eeb46389144448
chenhaizhu <394279480@qq.com>
2024-05-28 21:53:27 +0800

    添加遮罩层完成确认窗口

---
+++ react_study/04_Learn_Log/24_添加遮罩层完成确认窗口/App.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./App.css";
+import "./Components/UI/ConfirmModal/ConfirmModal.css";
+import Logs from "./Components/Logs/Logs";
+import LogsForm from "./Components/LogsForm/LogsForm";
+
+const App = () => {
+  // 数据
+  const [logsData, setLogsData] = useState([
+    {
+      id: "001",
+      date: new Date(2024, 4, 23, 12, 30),
+      desc: "学习vue",
+      time: 30,
+    },
+    {
+      id: "002",
+      date: new Date(2024, 5, 30, 18, 30),
+      desc: "学习react",
+      time: 30,
+    },
+    {
+      id: "003",
+      date: new Date(2024, 6, 15, 12, 40),
+      desc: "学习js",
+      time: 60,
+    },
+    {
+      id: "004",
+      date: new Date(2024, 6, 20, 15, 30),
+      desc: "学习html",
+      time: 30,
+    },
+  ]);
+
+  /**
+   * 下一步：
+   *    需要将LogsForm中的数据传递给App组件，然后由App组件，将新的日志添加到数组中
+   */
+
+  // 定义一个函数
+  const saveLogHandler = (newLog) => {
+    // 向新的日志中添加id
+    newLog.id = Date.now() + "";
+
+    // 将新数据添加到数组中
+    // logsData.push(newLog);
+    setLogsData([...logsData, newLog]);
+  };
+
+  // 定义一个函数用来从数据中删除一条记录
+  const delLogByIndex = (index) => {
+    setLogsData((prevState) => {
+      const newLogs = [...prevState];
+      newLogs.splice(index, 1);
+      return newLogs;
+    });
+  };
+
+  return (
+    <div className="app">
+      {/* 引入LogsForm */}
+      <LogsForm onSaveLog={saveLogHandler} />
+      <Logs logsData={logsData} onDelLog={delLogByIndex} />
+    </div>
+  );
+};
+
+export default App;
---
+++ react_study/04_Learn_Log/24_添加遮罩层完成确认窗口/Components/Logs/Logs.js
@@ -0,0 +1,47 @@
+import Card from "../UI/Card/Card";
+import LogItem from "./LogItem/LogItem";
+import "./Logs.css";
+
+// 日志容器
+const Logs = (props) => {
+  /**
+   * losData用来存储学习的日志
+   *    这个数据除了当前组件Logs需要使用外，LogsForm也需要使用
+   *    当遇到一个数据需要被多个组件使用时，我们可以将数据放入到这些组件共同的祖先元素中
+   *    这样就可以使得多个组件都能方便的访问到这个数据
+   *
+   * state的提升
+   */
+  // 模拟一组从服务器中加载的数据
+
+  // 将数据放入JSX中
+  let logItemData = props.logsData.map((value, index) => (
+    <LogItem
+      onDelLog={() => props.onDelLog(index)}
+      key={value.id}
+      date={value.date}
+      desc={value.desc}
+      time={value.time}
+    />
+    // <LogItem {...value} />
+  ));
+  if (logItemData.length === 0) {
+    logItemData = <p className="no-logs">没有找到日志！</p>;
+  }
+
+  return (
+    <Card className="logs">
+      {/* 在父组件中可以直接在子组件中设置属性 */}
+      {/* <LogItem test="123" hello="abc" fn={() => {}} /> */}
+      {/* <LogItem
+        date={new Date(2024, 5, 13)}
+        desc={"学习JavaScript"}
+        time={"50"}
+      />
+      <LogItem date={new Date(2024, 7, 5)} desc={"学习React"} time={"30"} /> */}
+      {logItemData}
+    </Card>
+  );
+};
+
+export default Logs;
