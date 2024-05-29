0bd5e183642db8115a13da6f862a17396e992c72
chenhaizhu <394279480@qq.com>
2024-05-29 22:42:02 +0800

    CSS Modules

---
+++ react_study/react-app/src/A.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./A.module.css";
+
+const A = () => {
+  return (
+    <div>
+      <p className={styles.p1}>我是A组件</p>
+    </div>
+  );
+};
+
+export default A;
